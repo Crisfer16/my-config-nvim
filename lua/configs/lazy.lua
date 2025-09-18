d5d0354eeea25c6299abd4d7ebedbe672d23e831
Crisfer16 <cristianfernandezlozano@gmail.com>
2025-09-18 21:42:39 +0200

    Config: Cargamos la config de la rama main a KDE
    - Cargamos la configuracón de la rama main a config-kde.
    - Modificamos el tema configurado por pywal.

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -142,19 +142,13 @@ return {
 			require("configs.whichkey")
 		end,
 	},
+
 	-- themes
 	{
-		"everviolet/nvim",
-		priority = 1000,
+		"AlphaTechnolog/pywal.nvim",
 		lazy = false,
 		config = function()
-			require("evergarden").setup({
-				theme = {
-					variant = "fall",
-					accent = "lavander",
-				},
-			})
-			vim.cmd("colorscheme evergarden")
+		require("pywal").setup()
 		end,
 	},
 }
